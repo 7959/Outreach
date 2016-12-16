8f10942b7d640a4fd46dc86aa9f0c0b6a0cce22a
Joseph Holdom <Joseph Holdom>
2016-12-15 22:16:43 -0500

    Started Airplanebot (Four wheel version)

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/AirplaneBotV1.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Joseph on 12/15/2016.
+ */
+
+@TeleOp(name = "V1",group = "Airplane Robot")
+public class AirplaneBotV1 extends OpMode {
+    private class Left {
+        private DcMotor front;
+        private DcMotor back;
+        public void init() {
+            front = hardwareMap.dcMotor.get("Front Left");
+            back = hardwareMap.dcMotor.get("Back Left");
+            front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            front.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            back.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        public void setPower(double speed) {
+            front.setPower(speed);
+            back.setPower(speed);
+        }
+    }
+    private class Right {
+        private DcMotor front;
+        private DcMotor back;
+        public void init() {
+            front = hardwareMap.dcMotor.get("Front Right");
+            back = hardwareMap.dcMotor.get("Back Right");
+            front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            front.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            back.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        public void setPower(double speed) {
+            front.setPower(speed);
+            back.setPower(speed);
+        }
+    }
+
+    private Left left;
+    private Right right;
+
+    public void init() {
+        left.init();
+        right.init();
+    }
+    public void loop() {
+        left.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x);
+        right.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x);
+    }
+}
